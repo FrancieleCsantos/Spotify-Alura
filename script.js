2eca616a209b9c329322a74d3ad6896125540b0f
FrancieleCSantos <francielecoceicaodossantos93@gmail.com>
2024-01-25 22:09:46 -0300

    Aula 4 imersão Alura

---
+++ script.js
@@ -0,0 +1,47 @@
+const searchInput = document.getElementById('search-input');
+const resultArtists = document.getElementById('result-artist');
+const playlistContainer = document.getElementById("result-playlists");
+const resultPlaylist = document.getElementById('result-playlists')
+
+
+function requestApi(searchTerm) {
+
+    const url = ` http://localhost:3000/artists?name_like=${searchTerm}`
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+
+}
+
+function displayResults(result) {
+
+    // resultPlaylist.classList.add('hidden');
+    hidePlaylists();
+    const artistName = document.getElementById('artist-name')
+    const artistImage = document.getElementById('artist-img')
+
+    result.forEach(element => {
+
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+
+    });
+
+    resultArtists.classList.remove('hidden');
+}
+
+function hidePlaylists() {
+    playlistContainer.classList.add("hidden");
+  }
+
+document.addEventListener('input', function () {
+
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === "") {
+        resultPlaylist.classList.add('hidden');
+        playlistContainer.classList.remove('hidden');
+        return;
+    }
+    requestApi(searchTerm);
+})
